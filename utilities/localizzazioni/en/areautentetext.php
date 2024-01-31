22719aff0dc0f42fb7d0f1644ee318e6c0c14cef
giocri <67639762+giocri@users.noreply.github.com>
2024-01-31 22:55:13 +0100

    bugfix

--- utilities/localizzazioni/en/areautentetext.php
+++ utilities/localizzazioni/en/areautentetext.php
@@ -3,6 +3,8 @@ function GetAreaUtenteText() : array {
     $areautente=[];
     $areautente["logout"]="log out";
     $areautente["in_as"]="currently logged in as";
+    $areautente["riepilogo_prenotazioni"]="bookings";
+    $areautente["modifica_prenotazioe"]="modify";
     return $areautente;
 }
 ?>
